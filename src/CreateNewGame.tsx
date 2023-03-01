0c9dfe66c649ccab85761c8edb2a5d0e6e778907
Jonathan Orman <jonathanpcorman@gmail.com>
2023-03-01 08:14:59 +0000

    Capture form data

--- src/CreateNewGame.tsx
+++ src/CreateNewGame.tsx
@@ -1,25 +1,31 @@
 import { writeTextFile, BaseDirectory } from '@tauri-apps/api/fs';
+import { useState } from 'react';
 
 function CreateNewGame() {
+    const [name, setName] = useState("");
+    const [sceneWidth, setSceneWidth] = useState(0);
+    const [sceneHeight, setSceneHeight] = useState(0);
+
+    const submit = async (e: any) => {
+        e.preventDefault();
+        const content = {
+            name: name,
+            scene: {
+                width: sceneWidth,
+                height: sceneHeight
+            },
+        };
+
+        const contentJson = JSON.stringify(content);
+
+        await writeTextFile('PATH_HERE/content.json', contentJson, { dir: BaseDirectory.AppConfig });
+    };
 
     return (
         <div>
             <h1>Create new game</h1>
             <form
-                onSubmit={async (e) => {
-                    e.preventDefault();
-                    const content = {
-                        name: "GAME NAME HERE",
-                        scene: {
-                            width: 10.0,
-                            height: 10.0
-                        },
-                    };
-
-                    const contentJson = JSON.stringify(content);
-
-                    await writeTextFile('PATH_HERE/content.json', contentJson, { dir: BaseDirectory.AppConfig });
-                }}
+                onSubmit={submit}
             >
                 <div>
                     <label>
@@ -27,6 +33,7 @@ function CreateNewGame() {
                         <input
                             id="name"
                             type="text"
+                            onChange={(e) => setName(e.currentTarget.value)}
                         />
                     </label>
                 </div>
@@ -37,6 +44,7 @@ function CreateNewGame() {
                         <input
                             id="scene-width"
                             type="number"
+                            onChange={(e) => setSceneWidth(+e.currentTarget.value)}
                         />
                     </label>
                 </div>
@@ -47,6 +55,7 @@ function CreateNewGame() {
                         <input
                             id="scene-height"
                             type="number"
+                            onChange={(e) => setSceneHeight(+e.currentTarget.value)}
                         />
                     </label>
                 </div>
