2aad281337d687e5b561ea56ba6d1c18b78640f8
Jonathan Orman <jonathanpcorman@gmail.com>
2023-06-21 21:09:37 +0100

    Use submitter in CreateEquilateralTriangle

--- src/content/triangle/EquilateralTriangle/CreateEquilateralTriangle.tsx
+++ src/content/triangle/EquilateralTriangle/CreateEquilateralTriangle.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import CreateContentProps from '../../CreateContentProps';
@@ -6,36 +6,37 @@ import NameEditor from '../../../fields/name/NameEditor';
 import DimensionEditor from '../../../fields/dimension/DimensionEditor';
 import EquilateralTriangle from './EquilateralTriangle';
 import { useEquilateralTriangleSubmitter } from './useEquilateralTriangleSubmitter';
+import { useSubmitter } from '../../useSubmitter';
 
 export default function CreateEquilateralTriangle(props: CreateContentProps) {
-  const equilateralTriangleSubmitter = useEquilateralTriangleSubmitter();
-
-  const [equilateralTriangle, setEquilateralTriangle] = useState<EquilateralTriangle>({
-    name: '',
-    type: 'EquilateralTriangle',
-    size: 0
-  });
+  const [submit, handleChange, value] = useSubmitter<EquilateralTriangle>({
+      name: '',
+      type: 'EquilateralTriangle',
+      size: 0
+    },
+    useEquilateralTriangleSubmitter(),
+  () => {});
 
   const onNameValueChange = (newNameValue: string) => {
-    setEquilateralTriangle({
-      name: newNameValue,
-    type: equilateralTriangle.type,
-    size: equilateralTriangle.size
+    handleChange<string>(newNameValue, (newNameValue: string) => {
+      return {
+        name: newNameValue,
+        type: value.type,
+        size: value.size
+      }
     });
   };
 
   const onSizeValueChange = (newSizeValue: number) => {
-    setEquilateralTriangle({
-      name: equilateralTriangle.name,
-    type: equilateralTriangle.type,
-    size: newSizeValue,
+    handleChange<number>(newSizeValue, (newSizeValue: number) => {
+      return {
+        name: value.name,
+        type: value.type,
+        size: newSizeValue,
+      }
     });
   };
-
-  const submit = async (e: any) => {
-    equilateralTriangleSubmitter.submit(equilateralTriangle, e, props.onHide);
-  };
-
+  
   return (
     <Form onSubmit={submit}>
       <NameEditor onChange={onNameValueChange}></NameEditor>
